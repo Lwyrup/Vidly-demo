4afad693ddac3d5f33220236d66a33a6ce21a485
Spencer Gerdes <skgerdes@yahoo.com>
2017-04-01 21:19:50 -0500

    Added methods all and byid to help divide responsabilities

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Vidly.Services;
 
 namespace Vidly.Models
 {
@@ -12,5 +13,21 @@ namespace Vidly.Models
 		public string Name        { get; set; }
 		public string Category    { get; set; }
 		public string Description { get; set; }
+
+		// Gets all the movies
+		//
+		// returns a List of Movie objects
+		public static List<Movie> All()
+		{
+			return Database.All();
+		}
+
+		// Gets a single movie based off an id number
+		//
+		// returns a Movie object
+		public static Movie ById(int id)
+		{
+			return Database.AllWithFilter(id);
+		}
 	}
 }

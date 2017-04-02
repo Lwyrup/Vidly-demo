1273d02fd83edbdaeb46a88f32780eae23cbacba
Spencer Gerdes <skgerdes@yahoo.com>
2017-04-01 21:18:39 -0500

    added show action

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -6,26 +6,23 @@ using System.Web.Mvc;
 using System.Web.Mvc.Ajax;
 using Vidly.Models;
 using Vidly.ViewModels;
-using Vidly.Helpers;
+using Vidly.Services;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-		// /movies/{name?}
-		public ActionResult Index(int? id)
+		public ActionResult Index()
 		{
-			// Get all the movies / or if and id is present, just that movie
-			var result = MoviesHelper.GetMovies(id);
-
-			// Insert the result into the view model
-			var viewModel = new MoviesViewModel
-			{
-				Movies = result,
-				CurrentMovieId = id
-			};
-
+			List<Movie> result = Movie.All();
+			var viewModel = new MoviesViewModel { Movies = result };
 			return View(viewModel);
         }
+
+		public ActionResult Show(int id)
+		{
+			var result = Movie.ById(id);
+			return View(result);
+		}
     }
 }

64bef1c2d558ecadb998676c19b08f4bf2d49ffe
Spencer Gerdes <skgerdes@yahoo.com>
2017-04-03 01:30:35 -0500

    Reduced controllers to simple actions

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Mvc.Ajax;
+﻿using System.Web.Mvc;
 using Vidly.Models;
-using Vidly.ViewModels;
-using Vidly.Services;
 
 namespace Vidly.Controllers
 {
@@ -14,22 +7,17 @@ namespace Vidly.Controllers
     {
 		public ActionResult Index()
 		{
-			List<Movie> result = Movie.All();
-			var viewModel = new MoviesViewModel { Movies = result };
-			return View(viewModel);
+			return View(MovieCollection.All());
         }
 
 		public ActionResult Show(int id)
 		{
-			var result = Movie.ById(id);
-			return View(result);
+			return View(MovieCollection.ById(id));
 		}
 
 		public ActionResult Search()
 		{
-			var result = Movie.Search(Request.Params);
-			var viewModel = new MoviesViewModel { Movies = result };
-			return View("Index", viewModel);
+			return View(MovieCollection.Search(Request.Params));
 		}
     }
 }

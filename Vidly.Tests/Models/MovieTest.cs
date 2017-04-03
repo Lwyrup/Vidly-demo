a90bf6aeee55043b1d8a8036c838165fdd4802e5
Spencer Gerdes <skgerdes@yahoo.com>
2017-04-02 21:15:09 -0500

    Wrote unit tests for models and services

---
+++ Vidly.Tests/Models/MovieTest.cs
@@ -0,0 +1,76 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Vidly;
+using Vidly.Models;
+
+namespace Vidly.Tests
+{
+	[TestFixture]
+	public class MovieTest
+	{
+		[Test]
+		public void All()
+		{
+			// Act
+			var result = Movie.All();
+			// Assert
+			Assert.IsNotEmpty(result);
+		}
+
+		[Test]
+		public void ById()
+		{
+			// Arrange
+			int id = 3;
+			// Act
+			var result = Movie.ById(id);
+			// Assert
+			Assert.IsInstanceOf<Movie>(result);
+			Assert.AreEqual(id, result.Id);
+		}
+
+		[Test]
+		public void ByName()
+		{
+			// Arrange
+			string nameSearch = "shrek";
+			// Act
+			var result = Movie.ByName(nameSearch);
+			// Assert
+			Assert.IsInstanceOf<List<Movie>>(result);
+			Assert.AreEqual(nameSearch, result[0].Name.ToLower());
+		}
+
+		[Test]
+		public void ByCat()
+		{
+			// Arrange
+			string categeory = "western";
+			// Act
+			var result = Movie.ByCat(categeory);
+			// Assert
+			Assert.GreaterOrEqual(result.Count(),2);
+			Assert.AreEqual(categeory, result[0].Category.ToLower());
+			Assert.AreEqual(categeory, result[1].Category.ToLower());
+		}
+
+		[Test]
+		public void Search()
+		{
+			// Arrange
+			NameValueCollection searchParams = new NameValueCollection();
+			searchParams.Add("searchBy","name");
+			searchParams.Add("value","kung fury");
+			// Act
+			var result = Movie.Search(searchParams);
+			// Assert
+			Assert.IsInstanceOf<List<Movie>>(result);
+			Assert.AreEqual(searchParams["value"], result[0].Name.ToLower());
+		}
+	}
+}
---
+++ Vidly.Tests/Services/DatabaseTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Vidly;
+using Vidly.Services;
+using Vidly.Models;
+
+namespace Vidly.Tests
+{
+	[TestFixture]
+	public class DatabaseTest
+	{
+		[Test]
+		public void All()
+		{
+			var result = Database.All();
+			Assert.IsNotEmpty(result);
+		}
+
+		[Test]
+		public void AllWithFilter()
+		{
+			// Arrange
+			del filter = (item) => item.Category.ToLowerInvariant().Contains("west");
+			// Act
+			var result = Database.AllWithFilter(filter);
+			//Assert
+			Assert.AreEqual(2, result.Count());
+		}
+	}
+}

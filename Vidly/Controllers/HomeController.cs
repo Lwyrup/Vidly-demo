d5edc6fbc98822878e12b47d966e56818af01f88
Spencer Gerdes <skgerdes@yahoo.com>
2017-04-03 01:30:54 -0500

    Removed comments

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -1,31 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Mvc.Ajax;
+﻿using System.Web.Mvc;
 
 namespace Vidly.Controllers
 {
 	public class HomeController : Controller
 	{
-		// / | /home
 		public ActionResult Index()
 		{
 			return View("Index");
 		}
 
-		// /about
 		public ActionResult About()
 		{
 			return View("About");
 		}
 
-		// /faq
 		public ActionResult Faq()
 		{
 			return View("Faq");
 		}
-
 	}
 }
